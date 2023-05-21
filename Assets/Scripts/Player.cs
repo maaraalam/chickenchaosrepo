6da70b5eaa91ea82edf411789624fb696f3fb85f
maaraalam <maaraalam@gmail.com>
2023-05-21 12:52:23 +0330

    animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    [Serialize] float moveSpeed = 7f;
-    // Update is called once per frame
+    [SerializeField] private float moveSpeed= 7f;
+    private bool isWalking;
     private void Update()
     {
+
+    
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.UpArrow))
         {
@@ -28,25 +25,34 @@ public class Player : MonoBehaviour
 
         if (Input.GetKey(KeyCode.LeftArrow))
         {
-            inputVector.x = +1;
+            inputVector.x = -1;
         }
 
         if (Input.GetKey(KeyCode.RightArrow))
         {
-            inputVector.x = -1;
+            inputVector.x = +1;
         }
         
         inputVector = inputVector.normalized;
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-        transform.position += moveDir*Time.deltaTime*moveSpeed;
-        float rotateSpeed = 10f;
-        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime*rotateSpeed);
+
+        transform.position += moveDir*moveSpeed*Time.deltaTime;
+        //transform.forward = moveDir;
+        isWalking = moveDir != Vector3.zero;
+        float rotateSpeed = 2f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
         //Debug.Log(inputVector);
-        // Debug.Log(Time.deltaTime);
+        //Debug.Log(Time.deltaTime);
+        Debug.Log(isWalking);
 
 
     }
+    public bool IsWalking()
+    {
+        return isWalking;
+    }
+
 
 }        
         
