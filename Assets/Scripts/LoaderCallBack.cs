20fb5e63ffa53981a03b9510f3310c25fa919769
maaraalam <maaraalam@gmail.com>
2023-08-05 13:49:23 +0330

    logoMenu

---
+++ Assets/Scripts/LoaderCallBack.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoaderCallBack : MonoBehaviour
+{
+    public bool isFirstUpdate = true;
+
+    private void Update()
+    {
+        if (isFirstUpdate)
+        {
+            isFirstUpdate = false;
+            Loader.LoaderCallBack();
+        }
+    }
+
+}
